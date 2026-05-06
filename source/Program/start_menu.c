7d9db8ec215f7d49493502d27eddcae1bc839651
Dimitris Panokostas <midwan@gmail.com>
2026-05-06 22:52:40 +0200

    chore: retire dopus5allamigas references in source headers + docs
    Project rename housekeeping for the 5.101 cycle: the historical
    'dopus5allamigas' fork name is dropped in favour of plain 'dopus5'
    (matching the current repo name BlitterStudio/dopus5).
    
    Sweep covers:
    
    - Source-file copyright headers in Library/, Misc/C/ViewFont/,
      Modules/filetype/, and most of Program/ (16 files):
      '(dopus5allamigas fork)' -> '(dopus5 fork)'.
    - dopus5.library + module init headers (8 files):
      'http://sourceforge.net/projects/dopus5allamigas/' ->
      'https://github.com/BlitterStudio/dopus5'.
    - AUTHORS: copyright line, support-site URL and bottom maintenance
      section updated; SourceForge URL retained as a 'historical
      project site' pointer for archaeology.
    - documents/DOpus5.guide: stray '(dopus5allamigas extension)'
      footer on the live-filter node updated to '(dopus5 fork
      extension)'; @$VER bumped 58.0 -> 58.1 with a v58.1 @remark
      describing the housekeeping.
    
    `grep -rIn dopus5allamigas source/` returns zero matches after this
    commit.  All remaining references in the tree (ChangeLog historical
    entries, README's 'historically known as' callout, README.legacy
    2012 release note, AUTHORS' historical-site pointer) are
    intentional.

--- source/Program/start_menu.c
+++ source/Program/start_menu.c
@@ -4,7 +4,7 @@ Directory Opus 5
 Original APL release version 5.82
 Copyright 1993-2012 Jonathan Potter & GP Software
 Copyright 2012-2013 DOPUS5 Open Source Team
-Copyright 2023-2026 Dimitris Panokostas (dopus5allamigas fork)
+Copyright 2023-2026 Dimitris Panokostas (dopus5 fork)
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the AROS Public License version 1.1.
